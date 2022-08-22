9fc2b3411c6ed768760918cfa9fcd95c2456e57d
Parth Songire <111558783+parth-songire23@users.noreply.github.com>
2022-08-22 12:10:21 +0530

    Stack class added, but using array.

--- main.cpp
+++ main.cpp
@@ -65,16 +65,73 @@ public:
     }
 };
 
-
+const int MAX=10000;
 class Stack
 {
-    Vector v;
-    int currsize;
+    int top;
     
+public:
     
+    int A[MAX];//MAX=10000
     
+    Stack()
+    {
+        top=-1;
+    }
     
+    bool push(int x);
+    int pop();
+    int peek();
+    bool IsEmpty();
 };
+
+bool Stack::push(int x)
+{
+    if(top>=MAX-1)
+    {
+        cout<<"Stack Overflow"<<endl;
+        return false;
+    }
+    else
+    {
+        A[++top]=x;
+        cout<<x<<" pushed into stack"<<endl;
+        return true;
+    }
+}
+
+int Stack::pop()
+{
+    if (top < 0)
+    {
+        cout << "Stack Underflow";
+        return 0;
+    }
+    else
+    {
+        int x = A[top--];
+        return x;
+    }
+}
+
+int Stack::peek()
+{
+    if (top < 0)
+    {
+        cout << "Stack is Empty";
+        return 0;
+    }
+    else
+    {
+        int x = A[top];
+        return x;
+    }
+}
+  
+bool Stack::IsEmpty()
+{
+    return (top < 0);
+}
 
 int main()
 {
