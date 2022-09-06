f6b4aacd08193850d92e3bd153cddc7923fd7e71
Parth Songire <111558783+parth-songire23@users.noreply.github.com>
2022-09-06 13:50:43 +0530

    main calculator readded

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LinkedList.hpp
+//
+//  Created by Parth Sunil Songire on 06/09/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
