954e9c48a3778e269b7dd70d8407290e4888431e
Parth Songire <111558783+parth-songire23@users.noreply.github.com>
2022-09-06 10:58:40 +0530

    Stack header file added

---
+++ stack.hpp
@@ -0,0 +1,30 @@
+//
+//  stack.hpp
+//  LinkedList.hpp
+//
+//  Created by Parth Sunil Songire on 06/09/22.
+//
+
+#ifndef stack_h
+#define stack_h
+
+#include "LinkedList.hpp"
+
+
+template <class T>
+class Stack
+{
+private:
+    Linkedlist<T> _stack;
+    
+public:
+    Stack(){};
+    bool isEmpty() { return _stack.size()==0;}
+    int size() { return _stack.size();}
+    T &top() { return _stack.tail();}
+    
+    void push(const T &e){_stack.append(e);}
+    T pop(){return _stack.remove_last();}
+};
+
+#endif /* stack_h */
